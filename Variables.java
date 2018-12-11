154d7c3d5b0e71f565fca54c44263ba13daef11a
Codigo-Libre <45804195+Codigo-Libre@users.noreply.github.com>
2018-12-11 17:10:11 -0600

    Variables | Java #4 | Datos Primitivos
    aqui esta el archivo del que les hable en le vídeo y también el .java

---
+++ Variables.java
@@ -0,0 +1,16 @@
+public class Variables{
+	public static void main(String[] args){
+		//ENTERO
+		byte b = 40;
+		short s = 1000;
+		int i = 32000;
+		long l = 50000;
+
+		//PUNTO FLOTANTE
+		float f = (float)1.4;
+		double d = 4434.3434334;
+
+		System.out.println(f);
+		System.out.println(d);
+	} 
+}
